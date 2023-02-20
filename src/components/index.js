8f73e3c2e4efa9d77f80f2cd31b335d6e1774bcc
imadghazaz <imad0business@gmail.com>
2023-02-20 18:22:56 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Button from "./Button";
+import FeedbackCard from "./FeedbackCard";
+
+export{
+    Navbar,
+    Footer,
+    GetStarted,
+    Button,
+    FeedbackCard
+}
